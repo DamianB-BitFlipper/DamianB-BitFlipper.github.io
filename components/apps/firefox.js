e633def672f07e0ed4958f6f97422057463d908a
Damian Barabonkov <damianb@alum.mit.edu>
2025-11-19 23:33:47 +0100

    fix: Update home URL to Wikipedia

--- components/apps/firefox.js
+++ components/apps/firefox.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 export class Firefox extends Component {
     constructor() {
         super();
-        this.home_url = 'https://www.google.com/webhp?igu=1';
+        this.home_url = 'https://www.wikipedia.org/';
         this.state = {
-            url: 'https://www.google.com/webhp?igu=1',
-            display_url: "https://www.google.com",
+            url: 'https://www.wikipedia.org/',
+            display_url: "https://www.wikipedia.org",
         }
     }
 
@@ -28,7 +28,7 @@ export class Firefox extends Component {
     }
 
     goToHome = () => {
-        this.setState({ url: this.home_url, display_url: "https://www.google.com" });
+        this.setState({ url: this.home_url, display_url: "https://www.wikipedia.org" });
         this.refreshFirefox();
     }
 
@@ -60,6 +60,11 @@ export class Firefox extends Component {
         this.setState({ display_url: e.target.value });
     }
 
+    handleBookmarkClick = (url, display_url) => {
+        this.setState({ url, display_url });
+        this.storeVisitedUrl(url, display_url);
+    }
+
     displayUrlBar = () => {
         return (
             <div className="w-full pt-0.5 pb-1 flex justify-start items-center text-white text-sm border-b border-gray-900">
@@ -74,10 +79,30 @@ export class Firefox extends Component {
         );
     }
 
+    displayBookmarks = () => {
+        const bookmarks = [
+            { name: 'Wikipedia', url: 'https://www.wikipedia.org/', display: 'https://www.wikipedia.org/' },
+            { name: 'Google', url: 'https://www.google.com/webhp?igu=1', display: 'https://www.google.com' },
+            { name: 'OpenStreetMap', url: 'https://www.openstreetmap.org/export/embed.html?bbox=-71.09902024269105%2C42.35640332139344%2C-71.08593106269838%2C42.36264255914027&amp;layer=mapnik', display: 'https://www.openstreetmap.org/?#map=17/42.359523/-71.092476' },
+            { name: 'MathWorld', url: 'https://mathworld.wolfram.com/', display: 'https://mathworld.wolfram.com/' },
+        ];
+
+        return (
+            <div className="w-full flex justify-start items-center text-white text-xs py-1 border-b border-gray-900 pl-2">
+                {bookmarks.map((bookmark, index) => (
+                    <div key={index} onClick={() => this.handleBookmarkClick(bookmark.url, bookmark.display)} className="cursor-pointer hover:bg-ub-grey px-2 py-1 rounded mr-1">
+                        {bookmark.name}
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
     render() {
         return (
             <div className="h-full w-full flex flex-col bg-ub-cool-grey">
                 {this.displayUrlBar()}
+                {this.displayBookmarks()}
                 <iframe src={this.state.url} className="flex-grow" id="firefox-screen" frameBorder="0" title="Ubuntu Firefox Url"></iframe>
             </div>
         )
