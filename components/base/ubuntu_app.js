854092c07f10c8a3ff92cfddb9f60f968c8932dc
Damian Barabonkov <damianb@alum.mit.edu>
2025-11-20 01:01:32 +0100

    feat: Add draggable Ubuntu app component

--- components/base/ubuntu_app.js
+++ components/base/ubuntu_app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Draggable from 'react-draggable';
 
 export class UbuntuApp extends Component {
 
@@ -12,24 +13,26 @@ export class UbuntuApp extends Component {
 
     render() {
         return (
-            <div
-                className="p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-ub-orange focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white relative"
-                id={"app-" + this.props.id}
-                onDoubleClick={this.openApp}
-                tabIndex={0}
-            >
-                <div className="relative">
-                    <img width="40px" height="40px" className="mb-1 w-10" src={this.props.icon} alt={"Ubuntu " + this.props.name} />
-                    {this.props.isExternalApp && (
-                        <img 
-                            src="./themes/Yaru/status/arrow-up-right.svg" 
-                            alt="External Link" 
-                            className="w-2.5 h-2.5 absolute -bottom-0.5 -right-0.5"
-                        />
-                    )}
+            <Draggable axis="both" grid={[25, 25]} bounds="parent">
+                <div
+                    className="p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-ub-orange focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white relative"
+                    id={"app-" + this.props.id}
+                    onDoubleClick={this.openApp}
+                    tabIndex={0}
+                >
+                    <div className="relative">
+                        <img width="40px" height="40px" className="mb-1 w-10" src={this.props.icon} alt={"Ubuntu " + this.props.name} />
+                        {this.props.isExternalApp && (
+                            <img 
+                                src="./themes/Yaru/status/arrow-up-right.svg" 
+                                alt="External Link" 
+                                className="w-2.5 h-2.5 absolute -bottom-0.5 -right-0.5"
+                            />
+                        )}
+                    </div>
+                    {this.props.name}
                 </div>
-                {this.props.name}
-            </div>
+            </Draggable>
         )
     }
 }
