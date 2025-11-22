1c51661ff7f5b837e5a05cb8041fae27606a1442
Damian Barabonkov <damianb@alum.mit.edu>
2025-11-22 18:35:40 +0100

    feat: Add touch device support to DesktopApp
    - Detects touch devices and disables dragging on them
    - Uses single click instead of double-click to open apps on touch
    - Updates state dynamically in componentDidMount for accuracy

--- components/base/desktop_app.js
+++ components/base/desktop_app.js
@@ -3,6 +3,43 @@ import Draggable from 'react-draggable';
 
 export class DesktopApp extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isTouchDevice: this.detectTouchDevice()
+        };
+    }
+
+    componentDidMount() {
+        const detected = this.detectTouchDevice();
+        if (detected !== this.state.isTouchDevice) {
+            this.setState({ isTouchDevice: detected });
+        }
+    }
+
+    detectTouchDevice = () => {
+        if (typeof window === 'undefined') return false;
+        const hasTouchEvents = 'ontouchstart' in window;
+        const nav = typeof navigator !== 'undefined' ? navigator : null;
+        const hasTouchPoints = nav ? ((nav.maxTouchPoints || 0) > 0 || (nav.msMaxTouchPoints || 0) > 0) : false;
+        const prefersCoarsePointer = typeof window.matchMedia === 'function' ? window.matchMedia('(pointer: coarse)').matches : false;
+        return hasTouchEvents || hasTouchPoints || prefersCoarsePointer;
+    }
+
+    isTouchEnvironment = () => {
+        if (this.state.isTouchDevice) return true;
+        const detected = this.detectTouchDevice();
+        if (detected && !this.state.isTouchDevice) {
+            this.setState({ isTouchDevice: true });
+        }
+        return detected;
+    }
+
+    handleClick = (event) => {
+        if (!this.isTouchEnvironment()) return;
+        this.openApp();
+    }
+
     openApp = () => {
         if (this.props.isExternalApp && this.props.url) {
             window.open(this.props.url, "_blank");
@@ -12,12 +49,14 @@ export class DesktopApp extends Component {
     }
 
     render() {
+        const { isTouchDevice } = this.state;
         return (
-            <Draggable axis="both" grid={[25, 25]} bounds="parent">
+            <Draggable axis="both" grid={[25, 25]} bounds="parent" disabled={isTouchDevice}>
                 <div
                     className="p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-ub-orange focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white relative"
                     id={"app-" + this.props.id}
-                    onDoubleClick={this.openApp}
+                    onDoubleClick={!isTouchDevice ? this.openApp : undefined}
+                    onClick={this.handleClick}
                     tabIndex={0}
                 >
                     <div className="relative">
